74a6696b669a64cb4d9d47835e7fa83d5ba30f55
dusengepeggy <dickinepeggy100@gmail.com>
2026-06-14 13:38:37 +0200

    project initialisation

---
+++ src/main/java/com/demo/ecrlab/EcrLabApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ecrlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcrLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcrLabApplication.class, args);
+    }
+
+}
